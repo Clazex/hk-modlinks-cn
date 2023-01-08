f5b59a6f6c1122924b08b069cc2ef6e694274534
Frank Yang <clazex.y@gmail.com>
2023-01-09 03:56:14 +0800

    feat: unify file format to zip

--- HKModLinksCN/Program.cs
+++ HKModLinksCN/Program.cs
@@ -1,4 +1,7 @@
-using System.Net.Http.Headers;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml;
 
@@ -13,13 +16,19 @@ List<Task> tasks = new();
 try {
 	Directory.Delete("dist", true);
 } catch { }
+try {
+	Directory.Delete("temp", true);
+} catch { }
+
 Directory.CreateDirectory("dist/apis");
 Directory.CreateDirectory("dist/mods");
+Directory.CreateDirectory("temp");
 
 #region Download and parse ApiLinks.xml
 
-XmlDocument apiLinksXml = new();
-apiLinksXml.PreserveWhitespace = true;
+XmlDocument apiLinksXml = new() {
+	PreserveWhitespace = true
+};
 using (HttpClient client = new()) {
 	apiLinksXml.Load(
 		client
@@ -32,8 +41,9 @@ using (HttpClient client = new()) {
 
 #region Download and parse ModLinks.xml
 
-XmlDocument modLinksXml = new();
-modLinksXml.PreserveWhitespace = true;
+XmlDocument modLinksXml = new() {
+	PreserveWhitespace = true
+};
 using (HttpClient client = new()) {
 	modLinksXml.Load(
 		client
@@ -64,10 +74,9 @@ IEnumerable<Task> apiDownloadTasks = apiLinkNodes
 			Stream res = task.Result;
 			Stream fileStream = File.Create($"dist/apis/{node.ParentNode.Name}.zip");
 			res.CopyTo(fileStream);
-			fileStream.Dispose();
-			res.Dispose();
+
+			Console.WriteLine($"Downloaded {node.ParentNode.Name} api");
 		})
-		.ContinueWith(_ => Console.WriteLine($"Downloaded {node.ParentNode.Name} api"))
 	);
 
 tasks = tasks.Concat(apiDownloadTasks).ToList();
@@ -91,46 +100,88 @@ foreach (XmlNode modInfo in modLinksXml.GetElementsByTagName("Manifest")) {
 			break;
 		}
 	}
+
 	string link = linkNode.InnerText;
 
-	if (skipList.Exists(i => link.Contains(i))) {
+	if (skipList.Exists(link.Contains)) {
 		continue;
 	}
 
 #pragma warning restore CS8600, CS8602
 
 	Task downloadModtask = new HttpClient()
-		.GetAsync(link)
+		.GetStreamAsync(link)
 		.ContinueWith(task => {
-			HttpResponseMessage res = task.Result;
+			string modName = Regex.Replace(name.Normalize(NormalizationForm.FormD), @"[^ -&(-_a-~]", "");
+			StringBuilder modNameBuilder = new(modName.Length);
+			Regex.Matches(modName, @"(?:[A-Z]?[a-z]+)|[A-Z]|\d+")
+				.Select(match => match.Value)
+				.ToList()
+				.ForEach(part => {
+					_ = modNameBuilder.Append(char.ToUpperInvariant(part[0]));
 
-			ContentDispositionHeaderValue? diposition = res.Content.Headers.ContentDisposition;
-			string fileName = diposition?.FileName ?? link[(link.LastIndexOf('/') + 1)..];
-			fileName = fileName.Trim();
-			fileName = fileName.StartsWith('"') ? fileName[1..] : fileName;
-			fileName = fileName.EndsWith('"') ? fileName[0..(fileName.Length - 1)] : fileName;
-			foreach (char c in Path.GetInvalidFileNameChars()) {
-				fileName = fileName.Replace(c, '_');
+					if (part.Length > 1) {
+						_ = modNameBuilder.Append(part[1..]);
+					}
+				});
+
+			modName = modNameBuilder.ToString();
+
+			if (!char.IsUpper(modName[0])) {
+				modName = modName.Length switch {
+					1 => char.ToUpperInvariant(modName[0]) + modName[1..],
+					_ => modName.ToUpperInvariant()
+				};
 			}
+
+			string version = modInfo["Version"]!.InnerText;
+			string fullModName = $"{modName}-v{version}";
+			string fileName = fullModName + ".zip";
+
 			linkNode.InnerText = urlBase + $"mods/{HttpUtility.HtmlEncode(fileName)}";
 
-			Stream resStream = res.Content.ReadAsStream();
-			using (Stream modFile = File.Create($"dist/mods/{fileName}")) {
-				resStream.CopyTo(modFile);
-			}
+			Stream resStream = task.Result;
+			using MemoryStream ms = new();
+			resStream.CopyTo(ms);
 			resStream.Dispose();
-		})
-		.ContinueWith(_ => Console.WriteLine($"Downloaded {name}"));
+
+			try {
+				_ = new ZipArchive(ms, ZipArchiveMode.Read, true);
+
+				ms.Position = 0;
+				using Stream modFile = File.Create($"dist/mods/{fileName}");
+				ms.CopyTo(modFile);
+			} catch (InvalidDataException) {
+				_ = Directory.CreateDirectory($"temp/{modName}");
+				using (Stream tempFile = File.Create($"temp/{modName}/{modName}.dll")) {
+					ms.CopyTo(tempFile);
+				}
+
+				ZipFile.CreateFromDirectory(
+					$"temp/{modName}",
+					$"dist/mods/{fileName}",
+					CompressionLevel.SmallestSize,
+					false
+				);
+
+				Console.WriteLine($"Compressed {name}");
+			}
+
+			if (name == modName) {
+				Console.WriteLine($"Downloaded {fullModName}");
+			} else {
+				Console.WriteLine($"Downloaded {name} as {fullModName}");
+			}
+		});
 
 	tasks.Add(downloadModtask);
 }
 
 #endregion
 
-foreach (Task task in tasks) {
-	task.Wait();
-}
+await Task.WhenAll(tasks);
 
+Directory.Delete("temp", true);
 apiLinksXml.Save("dist/ApiLinks.xml");
 modLinksXml.Save("dist/ModLinks.xml");
 
